42add5267cb6fc98a64e2ba78d1848f969b09f80
Kevin T <30321316+glaciyan@users.noreply.github.com>
2023-11-01 22:34:30 +0100

    use example

--- src/cc/glaciyan/Main.java
+++ src/cc/glaciyan/Main.java
@@ -2,29 +2,32 @@ package cc.glaciyan;
 
 import cc.glaciyan.ipt.IPTLexer;
 import cc.glaciyan.ipt.IPTParser;
+import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 
+import java.io.File;
+import java.io.FileInputStream;
+
 public class Main {
-    private final static String FILE = """
-            ipt = {
-            \tmode = "cut",
-            \tbase = {"zev_no02_1", x=-640, y=-360, w=2560, h=1440, ax=1280, ay=720 },
-            \t{ id=1, file="zev_no02_1_01", x=0, y=0 },
-            \t{ id=2, file="zev_no02_1_02", x=2048, y=0 },
-            }
-            """;
+    private final static String FILE_PATH = "./sample.ipt";
 
     public static void main(String[] args) {
-        IPTLexer lexer = new IPTLexer(CharStreams.fromString(FILE));
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        IPTParser parser = new IPTParser(tokens);
-        ParseTree tree = parser.start();
+        File f = new File(FILE_PATH);
+        try (var fileStream = new FileInputStream(f)) {
+            CharStream stream = CharStreams.fromStream(fileStream);
+            IPTLexer lexer = new IPTLexer(stream);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            IPTParser parser = new IPTParser(tokens);
+            ParseTree tree = parser.start();
 
-        IPTFileVisitor visitor = new IPTFileVisitor();
-        IPTAny output = visitor.visit(tree);
+            IPTFileVisitor visitor = new IPTFileVisitor();
+            IPTAny output = visitor.visit(tree);
 
-        System.out.println(output);
+            System.out.println(output);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
