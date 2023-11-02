65089da61b1f7008fc2bf7e4841adc45c676de76
Kevin T <30321316+glaciyan@users.noreply.github.com>
2023-11-02 10:14:46 +0100

    more flexible file reader

--- src/cc/glaciyan/IPTFile.java
+++ src/cc/glaciyan/IPTFile.java
@@ -9,13 +9,13 @@ import org.antlr.v4.runtime.tree.ParseTree;
 import java.io.IOException;
 
 public class IPTFile {
-    public static IPTAny read(CharStream stream) throws IOException {
+    public static IPTNamed.List read(CharStream stream) throws IOException {
         IPTLexer lexer = new IPTLexer(stream);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         IPTParser parser = new IPTParser(tokens);
         ParseTree tree = parser.start();
 
         IPTFileVisitor visitor = new IPTFileVisitor();
-        return visitor.visit(tree);
+        return (IPTNamed.List) visitor.visit(tree);
     }
 }
